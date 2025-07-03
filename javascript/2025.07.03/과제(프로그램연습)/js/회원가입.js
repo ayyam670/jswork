88d35a2b3054c3d63e3046be01b406c4984090c8
Jiwon H <ayyam670@naver.com>
2025-07-03 17:49:17 +0900

    0703 수업

---
+++ javascript/2025.07.03/과제(프로그램연습)/js/회원가입.js
@@ -0,0 +1,45 @@
+//input 상자 접근
+let uId = document.querySelector('#user-id');
+let Pw1 = document.querySelector('#user-pw1');
+let Pw2 = document.querySelector('#user-pw2');
+
+
+//입력상자에 값이 바뀌는 이벤트 발생하면 이벤트 핸들러 연결
+uId.onchange = idchc;
+Pw1.onchange = pwchc;
+Pw2.onchange = pwcompare;
+
+// id 길이 체크
+function idchc()
+{
+  let idlen = uId.value.length;
+  if(idlen < 3 || idlen > 10 )
+  {
+    alert('3글자 이상 10글자 이하입니다.')
+    uId.select();
+  }
+}
+
+//비밀번호 길이 체크
+function pwchc()
+{
+  let pwlen = Pw1.value.length;
+  if(pwlen < 4)
+  {
+    alert('4글자 이상입니다.')
+    Pw1.value = '';
+    Pw1.focus();
+  }
+}
+
+
+// 비밀번호가 맞는지 확인
+function pwcompare()
+{
+  if(Pw1.value != Pw2.value)
+  {
+    alert('비밀번호가 일치하지 않습니다.')
+    Pw2.value = '';
+    Pw2.focus();
+  }
+}
